ce99cd954f4085eb6a3ddfb8913a822dc6f78a41
dev0lt <igorinho07@o2.pl>
2023-12-19 19:36:46 +0100

    finished

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number < 0) return "OOPS";
+  let num = Number(number);
+  const fib = [0, 1];
+  for (let i = 2; i <= num; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib[num];
 };
 
 // Do not edit below this line
