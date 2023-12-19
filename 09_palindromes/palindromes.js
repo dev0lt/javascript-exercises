7d6f1642cd648e1af9844652f8d84d6e50483872
dev0lt <igorinho07@o2.pl>
2023-12-19 19:21:10 +0100

    finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  let wordLowerCase = word
+    .toLowerCase()
+    .split("")
+    .filter((w) => w.match(/[a-z0-9]/g))
+    .join("");
+  let reversedWord = wordLowerCase.split("").reverse().join("");
+  return wordLowerCase === reversedWord ? true : false;
 };
 
 // Do not edit below this line
